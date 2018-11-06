a0a285a7f8af02e26480674ce3e0c09e5687d780
ImanSyed <31649603+ImanSyed@users.noreply.github.com>
2018-11-06 22:48:28 +0530

    ah-9e3ryu2039ryu02po

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,7 +32,6 @@ public class GameManager : MonoBehaviour {
                 if(t.transform.position == u.transform.position)
                 {
                     t.hasUnit = true;
-                    return;
                 }
             }
         }
